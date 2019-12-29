3e542f3356910092d580b64f2675e3aebe2e0ee6
Tomek Wa <tomekwa@gmail.com>
2019-12-29 21:20:36 +0100

    filters

--- nbil/filters.py
+++ nbil/filters.py
@@ -4,14 +4,20 @@ from .models import UR_objects, UR_conn
 
 
 class URFilter(django_filters.FilterSet):
-    class Meta:
-        model = UR_objects
-        fields = ['name', 'code',]
+	name=django_filters.CharFilter(lookup_expr='icontains')
+	code=django_filters.CharFilter(lookup_expr='icontains')
+	class Meta:
+		model = UR_objects
+		fields = ['name', 'code',]
 
 
 class ConnFilter(django_filters.FilterSet):
-	POB = forms.ModelChoiceField(queryset=UR_objects.objects.filter(is_pob=0), to_field_name = 'name')
+	POB = forms.ModelChoiceField(queryset=UR_objects.objects.filter(name='qweqweq'), to_field_name = 'code')
+	#POB = forms.ModelChoiceField(queryset=UR_objects.objects.filter(is_pob=0), to_field_name = 'name')
 	SE = forms.ModelChoiceField(queryset=UR_objects.objects.filter(is_pob=1), to_field_name = 'name')
 	class Meta:
 		model = UR_conn
-		fields = ['POB', 'SE',]
+		fields = {'POB':['exact',],
+
+		'DT_from': ['range'],
+		}
